5c40fb9e6e2e6492dd0b505908442391b1d5b834
Mdub <mnw380@hotmail.com>
2017-11-12 14:47:29 -0500

    finished bstree delete and find functions, tested success

--- blockchain/src/main/scala/bstree.scala
+++ blockchain/src/main/scala/bstree.scala
@@ -14,13 +14,17 @@ class bstree[T](_t : T, lessThan : (T, T) => Boolean) {
   def insert(node : bstree[T]) : Unit = {
     if (true == lessThan(node.t, t)) {
 
-      if (null == left) left = node
-
+      if (null == left) {
+        left = node
+        node.parent = this
+      }
       else left.insert(node)
     }
 
-    else if (null == right) right = node
-
+    else if (null == right) {
+      right = node
+      node.parent = this
+    }
     else right.insert(node)
   }
 
@@ -29,6 +33,61 @@ class bstree[T](_t : T, lessThan : (T, T) => Boolean) {
     this.insert(node)
   }
 
+  def find(t : T) : bstree[T] = {
+    var r : bstree[T] = this
+    if (r.t == t) 
+      return r
+    else if (lessThan(t, r.t)) {
+      if (null != left)
+        return left.find(t)
+      else 
+        return null
+    }
+    else {
+      if (null != right) 
+        return right.find(t)
+      else 
+        return null
+    }
+  }
+
+  def delete() : bstree[T] = {
+    if (left != null && right != null) { // two children
+      var m = right.min()
+      if (null != parent) {
+        if (this == parent.left) parent.left = m
+        else parent.right = m
+      }
+      if (this != m.parent) m.parent.left = m.right
+      m.parent = parent
+      m.left = left
+      left.parent = m
+      return m
+    }
+    else if (left != null) { // left only
+      left.parent = parent
+      if (null != parent) {
+        if (this == parent.right) parent.right = left
+        else parent.left = left
+      }
+      return left
+    }
+    else if (right != null) { // right only
+      right.parent = parent
+      if (null != parent) {
+        if (this == parent.right) parent.right = right
+        else parent.left = right
+      }
+      return right
+    }
+    else // no children
+    {
+      if (this == parent.right) parent.right = null
+      else parent.left = null
+      return null
+    }
+  }
+ 
   def inorder(l : Array[T])(implicit m : ClassTag[T]) : Array[T] = {
     var tmp = l
     if (null != left) tmp = left.inorder(tmp)
@@ -58,5 +117,12 @@ class bstree[T](_t : T, lessThan : (T, T) => Boolean) {
       tmp = right.postorder(tmp)
 
     return tmp :+ t
+  }
+
+  private def min() : bstree[T] = {
+    if (left == null)
+      return this
+    else
+      return left.min()
   }
 }
--- blockchain/src/main/scala/bstree_test.scala
+++ blockchain/src/main/scala/bstree_test.scala
@@ -21,12 +21,43 @@ object bstree_test {
       root.insert(t)
     }
 
+    println("\nTrying to find random values...")
+    for (i <- 0 to numRand) {
+      r = rand.nextInt(randRange)
+      var x = root.find(r)
+      var t = if (null == x) -1 else x.t
+      print("root.find(" + r + ") : ")
+      while (x != null) {
+        print(" " + x.t)
+        x = x.parent
+      }
+      println()
+    }
+
     var l = Array[Int]()
     println("\ninorder " + root.inorder(l).mkString(" "))
     l = Array[Int]()
     println("preorder " + root.preorder(l).mkString(" "))
     l = Array[Int]()
     println("postorder " + root.postorder(l).mkString(" "))
+
+    println("\nDoing some random deletions and rerunning traversals")
+    for (i <- 0 to numRand << 1) {
+      r = rand.nextInt(randRange)
+      var x = root.find(r)
+      if (null != x) {
+        println("Deleting " + r);
+        if (root == x) root = x.delete()
+        else x.delete()
+        l = Array[Int]()
+        println("\ninorder " + root.inorder(l).mkString(" "))
+        l = Array[Int]()
+        println("preorder " + root.preorder(l).mkString(" "))
+        l = Array[Int]()
+        println("postorder " + root.postorder(l).mkString(" "))
+      }
+    }
+
 
   }
 }
